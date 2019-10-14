9edce1dfb951641fcf5ff68c064ee7e925cf7273
Ameer Jhan <ameerjhanprof@gmail.com>
2019-10-14 10:07:51 +0530

    feat: scrape top 250 movies

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-const cheerio = require('cheerio');
-const request = require('request');
+const cheerio = require("cheerio");
+const request = require("request");
 
-request.get("http://www.imdb.com/chart/top", (err, res) => {
-    if(!err && res.statusCode == 200) {
+const scrape = url => {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res) => {
+      if (!err && res.statusCode == 200) {
         const $ = cheerio.load(res.body);
-        console.log($("table").html());
-    }
-    else {
-        console.log(err);
-    }
-});
+        const movies = $("tbody.lister-list").children("tr");
+        const movieList = [];
 
+        for (let i = 0; i < movies.length; i++) {
+          const poster = $(movies[i])
+            .children("td.posterColumn")
+            .children("a")
+            .children("img")
+            .attr("src")
+            .replace("_UX45_", "_UX182_")
+            .replace("45,67", "182,268");
+          const name = $(movies[i])
+            .children("td.titleColumn")
+            .children("a")
+            .text();
+
+          const rating = $(movies[i])
+            .children("td.ratingColumn")
+            .children("strong")
+            .text();
+
+          movieList.push({
+            poster,
+            name,
+            rating
+          });
+        }
+
+        resolve(movieList);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
+
+scrape("http://www.imdb.com/chart/top").then(topMovieList => {
+  console.log(topMovieList);
+});
